41637cb3d13efb673a80d156656b881072337544
David Newhall II <david@sleepers.pro>
2019-02-10 01:38:53 -0800

    Add doc file.

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Provides a simple interface to set FFMPEG options and capture video from an RTSP
 Lots of other libraries out there do ffmpeg and rtsp things, but I couldn't find
 any that fit this simple task of "get a video snippet from a camera."
 
+- [GODOC](https://godoc.org/github.com/golift/ffmpeg)
+
 ## Example
 
 ```golang
--- encode_test.go
+++ encode_test.go
@@ -2,6 +2,7 @@ package ffmpeg
 
 import (
 	"fmt"
+	"log"
 	"strings"
 	"testing"
 
@@ -76,4 +77,49 @@ func TestValues(t *testing.T) {
 	a.EqualValues(DefaultCaptureSize, c.Size)
 	a.EqualValues(DefaultProfile, c.Prof)
 	a.EqualValues(DefaultLevel, c.Level)
+}
+
+/* GoDoc Code Examples */
+
+// Example non-transcode direct-save from securityspy.
+func Example_securitySpy() {
+	securitypsy := "rtsp://user:pass@127.0.0.1:8000/++stream?cameraNum=1"
+	output := "/tmp/securitypsy_captured_file.mov"
+	config := &Config{
+		FFMPEG: "/usr/local/bin/ffmpeg",
+		Copy:   true, // do not transcode
+		Audio:  true, // retain audio stream
+		Time:   10,   // 10 seconds
+	}
+	encode := Get(config)
+	cmd, out, err := encode.SaveVideo(securitypsy, output, "SecuritySpyVideoTitle")
+	log.Println("Command Used:", cmd)
+	log.Println("Command Output:", out)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Saved file from", securitypsy, "to", output)
+}
+
+// Example transcode from a Dahua IP camera.
+func Example_dahua() {
+	dahua := "rtsp://admin:password@192.168.1.12/live"
+	output := "/tmp/dahua_captured_file.m4v"
+	encode := Get(&Config{
+		Audio:  true, // retain audio stream
+		Time:   10,   // 10 seconds
+		Width:  1920,
+		Height: 1080,
+		CRF:    23,
+		Level:  "4.0",
+		Rate:   5,
+		Prof:   "baseline", // or main or high
+	})
+	cmd, out, err := encode.SaveVideo(dahua, output, "DahuaVideoTitle")
+	log.Println("Command Used:", cmd)
+	log.Println("Command Output:", out)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Saved file from", dahua, "to", output)
 }
